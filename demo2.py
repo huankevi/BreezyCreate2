1aa877f8bcf7ce77ac4c31dd017dc30a242d5c17
Kevin Huang <huankevi@amazon.com>
2017-11-11 21:53:06 -0800

    checked in latest version of demo2.py

--- demo2.py
+++ demo2.py
@@ -10,19 +10,18 @@ robot = breezycreate2.Robot(port='/dev/ttyUSB1')
 speed = lambda value: robot.setForwardSpeed(value)
 turn = lambda value: robot.setTurnSpeed(value)
 sleep = lambda value: time.sleep(value)
-direction = 1
 
-def u_turn(direction):
-	if direction > 0:
-        	print "turn clockwise"
-		step(0, 200, 1)
-		step(100, 0, 2)
-		step(0, 200, 1)
+def u_turn(d):
+	if d > 0:
+        	print "u-turn clockwise"
+		step(0, 250, 1)
+		step(100, 0, 1.5)
+		step(0, 155, 1)
 	else:
-        	print "turn counter-clockwise"
+        	print "u-turn counter-clockwise"
 		step(0, -200, 1)
-		step(100, 0, 2)
-		step(0, -200, 1)
+		step(100, 0, 1.5)
+		step(0, -165, 1)
 
 def step(speed_val, turn_val, time_val):
   if speed_val:
@@ -36,50 +35,49 @@ def step(speed_val, turn_val, time_val):
 def sensewalls(q):
    while True:
    	try:
-	#while True:
-	   print "=========================="
-    	   print "Bumper center left value: %s" % robot.getLightBumperCenterLeft()
-    	   #print "Bumper front left value: %s" % robot.getLightBumperFrontLeft()
-    	   #print "Bumper left value: %s" %  robot.getLightBumperLeft()
-    	   #print "=========================="
-    	   print "Bumper center right value: %s" % robot.getLightBumperCenterRight()
-    	   #print "Bumper front right value: %s" % robot.getLightBumperFrontRight()
-    	   #print "Bumper right value: %s" %  robot.getLightBumperRight()
-    	   print "=========================="
-           	        
-	   if robot.getLightBumperCenterLeft() > 80 or robot.getLightBumperCenterRight() > 80:
-		q.put("stop")
-		#speed(0)
-	   else:
-		q.put("move")
-		#step(50, 0, 2)
+		print "robot.getLightBumperLeft(): %s | robot.getLightBumperRight(): %s" % (robot.getLightBumperLeft(), robot.getLightBumperRight())
+	    	print "robot.getLightBumperFrontLeft(): %s | robot.getLightBumperFrontRight(): %s" % (robot.getLightBumperFrontLeft(), robot.getLightBumperFrontRight())
+	    	print "robot.getLightBumperCenterLeft(): %s | robot.getLightBumperCenterRight(): %s" % (robot.getLightBumperCenterLeft(), robot.getLightBumperCenterRight())
+	    	print "-----------------------------------------"
 
+	   	if (robot.getLightBumperCenterLeft() > 90 or robot.getLightBumperCenterRight() > 90):
+			print "stop!!!"
+			q.put("stop")
+			sleep(3.5)
+	   	else:
+			print "move!!!"
+			q.put("move")
    	except KeyboardInterrupt:
-        	speed(0)
 		break
    	except Exception, e:
 		print e
 
 def move(q):
-   sleep(1)
+   #sleep(1)
+   direction = -1
    while True:
 	try:
 	   if q.get() == "stop":
+		speed(0)
 		print "stop irobot"
-		#speed(0)
-		print "back away from the wall"
-		#step(-50, 0, 3)
-		speed(-200)
+		u_turn(direction)
+		direction = direction * -1
 	   elif q.get() == "move":
 		print "move forward"
-		#step(50, 0, 3)
 		speed(100)
+		#speed(0)
+		# take an image
+		# sleep (x)
 	   else:
-		print "OH NO! Stoppping"
-		print "What is in the queue: %s" % q.get()
 		speed(0)
+		print "STOP!!!! Turn around..."
+		if q.get() == "stop":
+			u_turn(direction)
+                	direction = direction * -1
+		else:
+			print "what is in queue 1: %s" % q.get()
+			print "What is in the queue 2: %s" % q.get()
 	except KeyboardInterrupt:
-		speed(0)
 		break
    	except Exception, e:
         	print e
@@ -103,6 +101,8 @@ if __name__ == '__main__':
 	process_one.terminate()
 	process_two.terminate()
 	while process_one.is_alive() and process_two.is_alive():
+		speed(0)
+		turn(0)
 		print process_one.is_alive()
 		print process_two.is_alive()
     except Exception, e:
